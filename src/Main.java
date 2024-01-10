8c12b5ea8e718aff7f0d4b300527158351ebc278
Duy Nguyen <duy.nguyen@netigo.de>
2024-01-10 19:03:02 +0100

    AVL Tree - Building + insert() + height

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import com.datastructure.noninear.AVLTree;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var tree = new AVLTree();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        System.out.println(tree);
     }
 }
---
+++ src/com/datastructure/noninear/AVLTree.java
@@ -0,0 +1,40 @@
+package com.datastructure.noninear;
+
+public class AVLTree {
+    private class AVLNode {
+        private int value;
+        private int height;
+        private AVLNode leftChild;
+        private AVLNode rightChild;
+
+        public AVLNode(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Value=" + this.value;
+        }
+    }
+    private AVLNode root;
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+    private AVLNode insert(AVLNode root, int value) {
+        if(root == null)
+            return new AVLNode(value);
+
+        if(value < root.value)
+            root.leftChild = insert(root.leftChild, value);
+        else
+            root.rightChild = insert(root.rightChild, value);
+
+        root.height = Math.max(height(root.leftChild), height(root.rightChild)) + 1;
+
+        return root;
+    }
+
+    private int height(AVLNode node){
+        return (node == null) ? -1 : node.height;
+    }
+}
